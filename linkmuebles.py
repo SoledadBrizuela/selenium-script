33ef666f6d38c721398dbc3bcdafaa1de7809c20
sole <sole.b_17@hotmail.com>
2022-12-18 23:15:19 -0300

    actualizado

--- linkmuebles.py
+++ linkmuebles.py
@@ -1,32 +1,67 @@
 import time
+import os 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import options
+from colorama import Fore, init, Back
 
+init(autoreset=True)
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('elxcludeSwitches',['enable-logging'])
 
 driver = webdriver.Chrome()
 driver.maximize_window()
+error=0
+
+#Abrir sitio web
 driver.get("https://tienda.centroestant.com.ar/")
 time.sleep(3)
 
-categoria = driver.find_element(By.ID,"menu-item-12954")
-categoria.click()
+#abrir link muebles
+mueble = driver.find_element(By.LINK_TEXT,"Muebles")
+mueble.click()
+MueblesNav = driver.find_element(By.XPATH,"//*[@id='menu-item-12954']/a").text
+
+if "Muebles" == MueblesNav:
+    print (Fore.CYAN + "Visibilidad de Muebles en el nav:" +Fore.GREEN+"PASS")
+else:
+    print (Fore.CYAN + "Visibilidad de Muebles en el nav:" +Fore.RED+"FAIL")
+    error+=1
+    
 time.sleep(3)
 
-producto = driver.find_element(By.LINK_TEXT,"Alacenas")
-producto.click()
+#seleccionar opcion alacenas
+alacena = driver.find_element(By.LINK_TEXT,"Alacenas")
+alacena.click()
+productoAlacena = driver.find_element(By.XPATH,"//*[@id='woocommerce_product_categories-2']/ul/li/ul/li[1]").text
+
+if "Alacenas" == productoAlacena:
+    print (Fore.CYAN + "Visibilidad de la opcion Alacenas:" +Fore.GREEN+"PASS")
+else:
+    print (Fore.CYAN + "Visibilidad de la opcion Alacenas:" +Fore.RED+"FAIL")
+    error+=1
+    
 time.sleep(2)
 
+#selecciona un tipo de alacena
 tipo = driver.find_element(By.LINK_TEXT,"Alacena 140")
 tipo.click()
+h1 = driver.find_element(By.XPATH,"//*[@id='product-14066']/div/div[1]/div/div[2]/h1" ).text
+
+
+if "Alacena 140" == h1:
+    print(Fore.CYAN + "Ingreso en el producto correcto:" +Fore.GREEN+"PASS")
+else:
+    print(Fore.CYAN + "Ingreso en el producto correcto:" +Fore.RED+"FAIL")
+    error+=1
+
 time.sleep(2)
 
-logo = driver.find_element(By.ID,"logo")
-logo.click()
+#vuelve a la homepage
+loguito = driver.find_element(By.ID,"logo")
+loguito.click()
 time.sleep(3)
-
+    
 driver.close()
-print("OK")
+
--- puntosventa.py
+++ puntosventa.py
@@ -1,30 +1,51 @@
 import time
+import os 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import options
 from selenium.webdriver.common.keys import Keys
+from colorama import Fore, init, Back
+
+init(autoreset=True)
 
 options = webdriver.ChromeOptions ()
 options.add_experimental_option ('elxcludeSwitches',['enable-logging'])
 
 driver = webdriver.Chrome()
 driver.maximize_window() 
+error=0
+
+#abrir sitio web
 driver.get ("https://centroestant.com.ar/")
 time.sleep(3)
 
-puntoventa = driver.find_element(By.ID,"menu-item-13488")
+#abre link muebles
+puntoventa = driver.find_element(By.LINK_TEXT,"Puntos de Venta")
 puntoventa.click()
+pventa = driver.find_element(By.LINK_TEXT,"Puntos de Venta")
+pventasNav = driver.find_element(By.XPATH,"//*[@id='menu-item-13488']/a").text
+
+if "Puntos de Venta" == pventasNav:
+    print (Fore.CYAN + "Visibilidad de Puntos de venta en el nav:" +Fore.GREEN+"PASS")
+else:
+    print (Fore.CYAN + "Visibilidad de Puntos de venta en el nav:" +Fore.RED+"FAIL")
+    error+=1
+    
 time.sleep(2)
 
-
+#abre cadenas oficiales
 cadenaopen = driver.find_element(By.LINK_TEXT,"Cadenas Oficiales").click() 
 time.sleep(2)
-cadenaopen = driver.find_element(By.LINK_TEXT,"Cadenas Oficiales").click() 
+cadenaclose = driver.find_element(By.LINK_TEXT,"Cadenas Oficiales").click() 
 time.sleep(2)
 
+
+
+#abre tienda centro estant oficial
 tiendaopen = driver.find_element(By.LINK_TEXT,"Tienda Centro Estant Oficial").click()
 time.sleep(2)
 
+#abre el link en otra pestaña
 link= driver.find_element(By.LINK_TEXT,"https://tienda.centroestant.com.ar/")
 link.send_keys(Keys.CONTROL + Keys.RETURN)
 driver.switch_to.window(driver.window_handles[1])
@@ -41,5 +62,5 @@ logo = driver.find_element (By.ID,"logo")
 logo.click()
 time.sleep(3)
 
+
 driver.close()
-print("OK")
